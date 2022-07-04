6bd51824eec0511f354720996838b949b8f81559
eduardareichow <103055053+eduardareichow@users.noreply.github.com>
2022-07-04 10:32:24 -0300

    Utilizando TreeSet

---
+++ Aula_02_05/ListaAluno.java
@@ -0,0 +1,19 @@
+package Aula_02_05;
+
+import java.util.Set;
+import java.util.TreeSet;
+	
+public class ListaAluno {
+		
+		public static void main(String[]args) {
+			
+			Set<String> Estudante = new TreeSet<>();
+			Estudante.add("Rhys");
+			Estudante.add("Cass");
+			Estudante.add("Amren");
+			Estudante.add("Az");
+			Estudante.add("Mor");
+			System.out.println(Estudante);
+		}
+
+}
